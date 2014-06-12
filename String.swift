f8345407427d5a8937dfd83d68193e0f9ec8d9a6
Matthew Purland <m.purland@gmail.com>
2014-06-11 17:06:48 -0700

    String extension for localizing strings

---
+++ String.swift
@@ -0,0 +1,12 @@
+extension String {
+    var localized: String {
+        return self.localized("")
+    }
+    func localized(comment:String) -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+}
+
+// Example for localizing strings easier.
+
+var localizedHelloWorld = "Hello World".localized
