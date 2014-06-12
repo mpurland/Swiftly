1089cd9ef3303f6c487b85e5fcdc550b3bea8854
Matthew Purland <m.purland@gmail.com>
2014-06-11 17:20:42 -0700

    Label for localized with comment
    Additional shorthand for without comment label

--- String.swift
+++ String.swift
@@ -1,13 +1,17 @@
 extension String {
     var localized: String {
-        return self.localized("")
+        return self.localized(comment: "")
     }
     func localized(comment:String) -> String {
-        return NSLocalizedString(self, comment: comment)
+        return self.localized(comment: comment)
+    }
+    func localized(comment commentString:String) -> String {
+        return NSLocalizedString(self, comment: commentString)
     }
 }
 
 // Example for localizing strings easier.
 
 var localizedHelloWorld = "Hello World".localized
+localizedHelloWorld = "Hello World".localized(comment: "Title for Hello World")
 localizedHelloWorld = "Hello World".localized("Title for Hello World")
